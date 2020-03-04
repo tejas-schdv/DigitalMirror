1f1295287f2d89c645c405168c3333ef26c0f9cc
Absalon Sanchez <absalon109@gmail.com>
2020-03-03 16:21:31 -0800

    Weather Module functional
    weather module works for hardcoded location, still need to implement a dynamic solution

--- app/src/main/java/com/example/digitalmirror/MainActivity.java
+++ app/src/main/java/com/example/digitalmirror/MainActivity.java
@@ -1,35 +1,88 @@
 package com.example.digitalmirror;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.androdocs.httprequest.HttpRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    //weather variables
+    DatabaseReference database;
+    String CITY = "dhaka,bd";
+    String API = "ebe86c447a46a73e12d63b0def7ba170";
+    ImageView ivWeather, ivWind;
+    TextView tvStatus, tvTemp, tvTempMin, tvTempMax, tvWind;
+
+    Button btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String currentTime = java.text.DateFormat.getTimeInstance().format(new Date());
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        Button btnSettings;
 
+        ivWeather = findViewById(R.id.ivWeather);
+        ivWind = findViewById(R.id.ivWind);
 
+        tvStatus = findViewById(R.id.tvStatus);
+        tvTemp = findViewById(R.id.tvTemp);
+        tvTempMax = findViewById(R.id.tvTempMax);
+        tvTempMin = findViewById(R.id.tvTempMin);
+        tvWind = findViewById(R.id.tvWind);
         btnSettings = findViewById(R.id.btnSettings);
+
+        new weatherTask().execute();
+
+        //clock variables
+        final String currentTime = java.text.DateFormat.getTimeInstance().format(new Date());
+
+
+
 
         //if() //if clock checkbox is checked
         TextClock clock;
         clock = findViewById(R.id.tvClock);
         clock.setText(currentTime);
+        database = FirebaseDatabase.getInstance().getReference().child("modules").child("clock").child("time");
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!currentTime.equals(dataSnapshot.getValue().toString()))
+                {
+                    database.setValue(currentTime);
+                    //clock.setText(currentTime);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
@@ -40,5 +93,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+                /*findViewById(R.id.loader).setVisibility(View.VISIBLE);
+                findViewById(R.id.mainContainer).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.GONE);*/
+        }
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                Long sunrise = sys.getLong("sunrise");
+                Long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                tvStatus.setText(weatherDescription.toUpperCase());
+                tvTemp.setText(temp);
+                tvTempMin.setText(tempMin);
+                tvTempMax.setText(tempMax);
+                tvWind.setText(windSpeed);
+                //addressTxt.setText(address);
+                //updated_atTxt.setText(updatedAtText);
+                //sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                //sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                //pressureTxt.setText(pressure);
+                //humidityTxt.setText(humidity);
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                //findViewById(R.id.loader).setVisibility(View.GONE);
+                //findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+
+
+            } catch (JSONException e) {
+                Toast toast=Toast.makeText(getApplicationContext(),"ERROR",Toast.LENGTH_SHORT);
+                toast.show();
+                //findViewById(R.id.loader).setVisibility(View.GONE);
+                //findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+            }
+
+        }
     }
 }
